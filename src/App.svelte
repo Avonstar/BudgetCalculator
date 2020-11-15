a8e355ecb258866aa92691a7d83135d1fd097a6a
R Adams <meisterthea@gmail.com>
2020-11-15 22:48:25 +0000

    Including reactivity

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,44 @@
 <script>
+	import { setContext } from 'svelte';
+
+	// components
 	import Navbar from './Navbar.svelte';
-	import Title from './Title.svelte';
+	import ExpensesList from './ExpensesList.svelte';
+	import Totals from './Totals.svelte';
+	import ExpenseForm from './ExpenseForm.svelte';
+
+	// data
+	import expensesData from './expenses';
+
+	// variables
+	let expenses = [...expensesData];
+	
+	// reactive
+	$: total = expenses.reduce((acc, curr) => {
+		console.log({acc, amount: curr.amount})
+		return (acc += curr.amount)
+	}, 0);
+
+	// functions
+	function removeExpense(id){
+		expenses = expenses.filter(item => item.id !== id);
+	}
+
+	function clearExpenses() {
+		expenses=[];
+	}
+
+	// context
+	setContext('remove', removeExpense);
+	
 </script>
 
 <Navbar/>
-<Title/>
+<main class="content">
+	<ExpenseForm/>
+	<Totals title="total expenses" {total} />
+	<ExpensesList {expenses} />
+	<button type="button" class="btn btn-primary btn-block" on:click={clearExpenses}>
+		clear expenses
+	</button>
+</main>
